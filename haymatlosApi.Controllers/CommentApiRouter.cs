2ca30b8327867f177078bddd22c3320a21e3104a
baris <legendcrafter24@gmail.com>
2023-08-24 11:23:58 +0300

    fixed comment endpoint.

--- haymatlosApi.Controllers/CommentApiRouter.cs
+++ haymatlosApi.Controllers/CommentApiRouter.cs
@@ -1,7 +1,9 @@
 ﻿using haymatlosApi.haymatlosApi.Models;
+using haymatlosApi.haymatlosApi.Utils;
 using haymatlosApi.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System.Data;
 
 namespace haymatlosApi.Controllers
@@ -17,11 +19,12 @@ namespace haymatlosApi.Controllers
         //POST:
         [HttpPost]
         [Authorize(Roles = "user")]
-        public async Task/*<Comment>*/ createComment(Post post, Comment comment, Guid? parentComment)
+        public async Task/*<Comment>*/ createComment(JObject postAndCommentDatafromJson, Guid? parentComment)
         {
-            await _commentService.createComment(post, comment, parentComment);
+            var post_and_comment = objectsFromJsonData.PostAndComment(postAndCommentDatafromJson);
+            await _commentService.createComment(post_and_comment.Item1, post_and_comment.Item2, parentComment);
         }
-
+        //i did this bcs turns out the endpoint doesn't like two different complicated classes as parameters. I had to send them from the JObj but this will be improved. just making it work for now.
         [HttpPut]
         [Authorize(Roles = "admin,user")]
         public async Task deleteComment(Guid commentId)
